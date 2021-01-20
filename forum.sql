f9c4a90d1a9886854e99b269db22eb1e350796ae
glad2os <45284275+glad2os@users.noreply.github.com>
2021-01-20 09:34:48 +0400

    Add files via upload

--- forum.sql
+++ forum.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Дек 30 2020 г., 09:21
--- Версия сервера: 8.0.20
--- Версия PHP: 7.4.7
+-- Хост: 127.0.0.1
+-- Время создания: Янв 20 2021 г., 06:34
+-- Версия сервера: 10.4.17-MariaDB
+-- Версия PHP: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,29 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `reactions`
+--
+
+CREATE TABLE `reactions` (
+  `thread_id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `threads`
 --
 
 CREATE TABLE `threads` (
-  `id` int NOT NULL,
-  `owner_id` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL,
   `title` text NOT NULL,
   `text` text NOT NULL,
-  `moderated` tinyint(1) NOT NULL DEFAULT '0',
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `moderated` tinyint(1) NOT NULL DEFAULT 0,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `threads`
---
-
-INSERT INTO `threads` (`id`, `owner_id`, `title`, `text`, `moderated`, `time`) VALUES
-(1, 2, 'фффффффф', '        Текст\r\n    ', 1, '2020-12-30 08:15:18'),
-(2, 2, 'ффффффффффф', '        Текст\r\n    ', 1, '2020-12-30 08:15:21'),
-(3, 2, 'ayakla', '        Текст\r\n    ', 1, '2020-12-30 08:18:01'),
-(4, 2, 'ayakla', '        Текст\r\n    ', 1, '2020-12-30 08:18:05'),
-(5, 2, 'ayakla', '        Текст\r\n    ', 1, '2020-12-30 08:18:45'),
-(6, 2, 'ayakla', '        Текст\r\n    ', 1, '2020-12-30 08:21:32');
 
 -- --------------------------------------------------------
 
@@ -55,22 +56,21 @@ INSERT INTO `threads` (`id`, `owner_id`, `title`, `text`, `moderated`, `time`) V
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `login` text NOT NULL,
   `passwd` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `users`
---
-
-INSERT INTO `users` (`id`, `login`, `passwd`) VALUES
-(1, 'test', '123'),
-(2, 'test2', '202cb962ac59075b964b07152d234b70');
-
---
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `reactions`
+--
+ALTER TABLE `reactions`
+  ADD KEY `author_id` (`author_id`),
+  ADD KEY `thread_id` (`thread_id`);
 
 --
 -- Индексы таблицы `threads`
@@ -94,23 +94,30 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `threads`
 --
 ALTER TABLE `threads`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
+-- Ограничения внешнего ключа таблицы `reactions`
+--
+ALTER TABLE `reactions`
+  ADD CONSTRAINT `reactions_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `reactions_ibfk_2` FOREIGN KEY (`thread_id`) REFERENCES `threads` (`id`);
+
+--
 -- Ограничения внешнего ключа таблицы `threads`
 --
 ALTER TABLE `threads`
-  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
